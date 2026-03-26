8656f593d56b290355be950c09453cdc5dce1e06
Veronika Shcherbyna <koraf9712@gmail.com>
2026-03-26 15:09:24 -0600

    created base game screen

---
+++ src/GameScreen.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class GameScreen {
+    public void runGameScreen() {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Very interesting game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    static void main() {
+        GameScreen gameScreen = new GameScreen();
+        gameScreen.runGameScreen();
+    }
+}
